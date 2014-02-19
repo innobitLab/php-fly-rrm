4d4cc263229b549642a5374a71094b8ed3e8f36e
Gabriele Tondi <info@gabrieletondi.it>
2014-02-19 18:56:30 +0100

    number formatter format null to null

--- src/FlyRRM/Formatting/Field/NumberFieldFormatter.php
+++ src/FlyRRM/Formatting/Field/NumberFieldFormatter.php
@@ -5,6 +5,10 @@ class NumberFieldFormatter
 {
     public function format($rawValue)
     {
+        if ($rawValue === null) {
+            return null;
+        }
+
         if (!is_numeric($rawValue)) {
             throw new \InvalidArgumentException();
         }
--- tests/FlyRRM/Tests/Formatting/Field/NumberFieldFormatterTest.php
+++ tests/FlyRRM/Tests/Formatting/Field/NumberFieldFormatterTest.php
@@ -5,19 +5,31 @@ use FlyRRM\Formatting\Field\NumberFieldFormatter;
 
 class NumberFieldFormatterTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var \FlyRRM\Formatting\Field\NumberFieldFormatter */
+    private $formatter;
+
+    protected function setUp()
+    {
+        $this->formatter = new NumberFieldFormatter();
+    }
+
     /**
      * @expectedException \InvalidArgumentException
      */
     public function test_that_a_number_field_formatter_require_a_number()
     {
-        $formatter = new NumberFieldFormatter();
-        $formatter->format('hello world');
+        $this->formatter->format('hello world');
+    }
+
+    public function test_that_null_is_formatted_to_null()
+    {
+        $this->assertEquals(null, $this->formatter->format(null));
     }
 
     public function test_that_a_number_is_correctly_formatted()
     {
-        $formatter = new NumberFieldFormatter();
-        $this->assertEquals('1250.53', $formatter->format(1250.53));
+
+        $this->assertEquals('1250.53', $this->formatter->format(1250.53));
     }
 
 }
